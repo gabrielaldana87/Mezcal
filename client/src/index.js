0d3994b5f328977a73b08d230d4b5fa8b71657dc
gabrielaldana87 <colognia5>
2020-01-08 19:51:55 -0500

    adding styles and client.js to versioning

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { BrowserRouter } from 'react-router-dom';
+import rootReducer from './reducers';
 import 'bootstrap/dist/css/bootstrap.css';
 import './styles/index.scss';
+import 'react-id-swiper/lib/styles/scss/swiper.scss';
+import 'rheostat/css/rheostat.css';
+import persistMiddleware from './middleware/persistMiddleware';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const preloadedState = window.PRELOADED_STATE;
+delete window.PRELOADED_STATE;
+
+const store = createStore(rootReducer, preloadedState,
+  composeWithDevTools(applyMiddleware(persistMiddleware))
+);
+
+ReactDOM.hydrate(
+  <Provider store={ store }>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
