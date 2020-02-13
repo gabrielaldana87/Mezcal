67144b0480930e675e66d2c8e90c88b670dfc98f
gabrielaldana87 <colognia5>
2020-02-13 14:49:30 -0500

    test

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,20 +1,33 @@
 const express = require('express');
 const router = express.Router();
-const gmail = require('../index');
 const configurePassport =  require('../config/passport');
+const callGoogleApi = require('../config/google');
+const groupLabelCategories = require('../lib/groupLabelCategories');
+const goalData = require('../lib/goalClassifications.json');
+const extractMessagesToCreateBoard = require('../lib/extractMessagesToCreateBoard');
+const data = require('../lib/goalClassifications');
 /* Connect to Google API */
 
 const api = (db) => {
   "use strict";
-  router.get('/boards' , (req, res) => gmail((err, boards) => {
-    res.send(boards);
-  }));
+  router.get('/boards' , (req, res) => callGoogleApi(auth => {
+    extractMessagesToCreateBoard(auth, boards => res.send(boards));
+  }))
+  ;
+  router.get('/categories', (req, res) => callGoogleApi( auth => {
+    groupLabelCategories(auth, data => res.send(data)) ;
+  }))
+  ;
   router.get('/user', (req, res) => configurePassport(db, (err, user) => {
     res.send(user);
-  }));
+  }))
+  ;
+  router.get('/goals/:label_id', (req, res) => {
+    const id = req.params.label_id;
+    res.send(goalData['goal_classifications'].filter(o => o['id'] === id));
+  });
   return router
   ;
 };
-
 
 module.exports = api;
